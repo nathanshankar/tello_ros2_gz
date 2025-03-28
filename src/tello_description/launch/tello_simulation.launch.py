13892adc2a2b2e85f781a7300572e5677b5927f3
Nathan Shankar <nathanshankar465@gmail.com>
2025-03-28 21:12:57 +0000

    deleted:    src/pcl_voxelizer/CMakeLists.txt 	deleted:    src/pcl_voxelizer/package.xml 	modified:   src/tello_description/launch/tello_simulation.launch.py 	modified:   src/tello_description/rviz/ryze_view.rviz 	modified:   src/tello_description/urdf/tello.urdf.xacro

--- src/tello_description/launch/tello_simulation.launch.py
+++ src/tello_description/launch/tello_simulation.launch.py
@@ -78,7 +78,8 @@ def generate_launch_description():
         namespace='',
         executable='rviz2',
         name='rviz2',
-        arguments=['-d' + os.path.join(get_package_share_directory(pkg_name), 'rviz', 'ryze_view.rviz')]
+        arguments=['-d' + os.path.join(get_package_share_directory(pkg_name), 'rviz', 'ryze_view.rviz')],
+        additional_env={'LD_PRELOAD': '/usr/lib/x86_64-linux-gnu/liboctomap.so'}
     )
 
     node_ros_gz_bridge = Node(
@@ -87,32 +88,54 @@ def generate_launch_description():
         arguments=  [
                     '/clock'                           + '@rosgraph_msgs/msg/Clock'   + '[' + 'gz.msgs.Clock',
                     '/world/empty/model/tello/joint_state' + '@sensor_msgs/msg/JointState' + '[' + 'gz.msgs.Model',
-                    '/ircam1'                               + '@sensor_msgs/msg/Image'      + '[' + 'ignition.msgs.Image',
-                    '/ircam1/points'                    + '@sensor_msgs/msg/PointCloud2' + '[' + 'ignition.msgs.PointCloudPacked',
+                    # '/ircam1'                               + '@sensor_msgs/msg/Image'      + '[' + 'ignition.msgs.Image',
+                    # '/ircam1/points'                    + '@sensor_msgs/msg/PointCloud2' + '[' + 'ignition.msgs.PointCloudPacked',
                     '/tello/cmd_vel'                    + '@geometry_msgs/msg/Twist'   + '@' + 'gz.msgs.Twist',
                     '/tello/imu'                  + '@sensor_msgs/msg/Imu'       + '[' + 'gz.msgs.IMU',
                     '/tello/odometry'                  + '@nav_msgs/msg/Odometry'    + '[' + 'gz.msgs.Odometry',
-                    '/tello/tf'                 + '@tf2_msgs/msg/TFMessage'    + '[' + 'gz.msgs.Pose_V',                    
+                    '/tello/tf'                 + '@tf2_msgs/msg/TFMessage'    + '[' + 'gz.msgs.Pose_V',
+                    '/world/empty/model/tello/link/base_link/sensor/rs_front/camera_info' + '@sensor_msgs/msg/CameraInfo' + '[' + 'gz.msgs.CameraInfo',  
+                    '/world/empty/model/tello/link/base_link/sensor/rs_front/image' + '@sensor_msgs/msg/Image' + '[' + 'gz.msgs.Image',
+                    '/world/empty/model/tello/link/base_link/sensor/rs_front/depth_image' + '@sensor_msgs/msg/Image' + '[' + 'gz.msgs.Image',
+                    '/world/empty/model/tello/link/base_link/sensor/rs_front/points' + '@sensor_msgs/msg/PointCloud2' + '[' + 'gz.msgs.PointCloudPacked',            
                     ],
         parameters= [{'qos_overrides./tello.subscriber.reliability': 'reliable'},{'qos_overrides./tello.subscriber.durability': 'transient_local'}],
         remappings= [
                     ('/world/empty/model/tello/joint_state', 'joint_states'),
-                    ('/ircam1', '/ircam1/image_raw'),
-                    ('/ircam1/points', '/points'),
+                    # ('/ircam1', '/ircam1/image_raw'),
+                    # ('/ircam1/points', '/points'),
                     ('/tello/cmd_vel', '/cmd_vel'),
                     ('/tello/imu', '/imu'),
                     ('/tello/odometry', '/odom'),
-                    ('/tello/tf', '/tf')  
+                    ('/tello/tf', '/tf'),
+                    ('/world/empty/model/tello/link/base_link/sensor/rs_front/camera_info', '/camera_info'),
+                    ('/world/empty/model/tello/link/base_link/sensor/rs_front/image', '/image_raw'),
+                    ('/world/empty/model/tello/link/base_link/sensor/rs_front/depth_image', '/depth_image'),
+                    ('/world/empty/model/tello/link/base_link/sensor/rs_front/points', '/points'),
                     ],
         output='screen'
     )
+    node_octomap_server = Node(
+        package='octomap_server',
+        executable='octomap_server_node',
+        name='octomap_server_node',
+        output='screen',
+        remappings=[
+            ('/cloud_in', '/points')
+        ],
+        parameters=[
+            {'frame_id': 'odom'}
+        ]
+    )
     
-    
+
+
     # Add actions to LaunchDescription
     ld.add_action(SetParameter(name='use_sim_time', value=True))
     ld.add_action(gz_start_world)
     ld.add_action(node_spawn_entity)
     ld.add_action(node_robot_state_publisher)
     ld.add_action(node_ros_gz_bridge)
+    ld.add_action(node_octomap_server)
     ld.add_action(node_rviz)
     return ld
